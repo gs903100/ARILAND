2f1a4331ea6c974eb84dddf5a3dfbe87976bf6ae
yeeunkim <ye903100@naver.com>
2016-11-16 15:13:11 +0900

    영범아 이제 마지막이야

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+  
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   get 'welcome/index'
   get 'about' => 'about#new'
   resources :poems do
     resources :poem_comments
   end
+  resources :stories do
+    resources :stories_comments
+  end 
+  resources :photos do
+  end  
   root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
